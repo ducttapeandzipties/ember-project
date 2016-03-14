7a1285b43893ae2e64a3e006dc64950a96bc5b2e
Dan <ducttapeandzipties@gmail.com>
2016-03-13 22:51:59 -0400

    got flickr api working with ember!

---
+++ app/controllers/about.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  init: function() {
+    console.log('init function fired');
+
+    var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var flickrOptions =
+
+      {
+        tags: "automotive",
+        format: "json"
+      };
+
+      function displayFlickr(data) {
+        var photoHTML = '<ul>';
+        $.each(data.items, function(li, photo){
+          photoHTML += '<li>';
+          photoHTML += '<a href= "' + photo.link + '">';
+          photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });   // end
+
+    photoHTML += '</ul>';
+    $('#photos').html(photoHTML);
+  }
+    $.getJSON(flickrAPI, flickrOptions, displayFlickr);
+
+  },
+  actions: {
+    tester: function(){
+      console.log('about js function fired');
+    }
+  }
+
+});
